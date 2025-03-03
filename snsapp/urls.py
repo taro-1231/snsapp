931f366e51f5a8ef3c76e72ddc8ba37472ad1d35
taro-1231 <okajimaryota1231@gmail.com>
2025-03-04 06:40:04 +0900

    initial commit

--- snsapp/urls.py
+++ snsapp/urls.py
@@ -1,5 +1,6 @@
 
 from django.urls import path
+from django.views.generic import RedirectView
 from .views import signup_func,login_func,list_func,logout_func,detail_func,good_func,read_func,create_func
 
 urlpatterns = [
@@ -11,4 +12,6 @@ urlpatterns = [
     path('good/<int:pk>', good_func, name='good'),
     path('read/<int:pk>', read_func, name='read'),
     path('create/', create_func.as_view(), name='create'),
+    path('',RedirectView.as_view(pattern_name='list',permanent=True)),
+
 ]
